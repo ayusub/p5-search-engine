730498cfd801e193380c14e4c82b61e1ce6abc13
Ayusha Subedi <ayusub@umich.edu>
2024-11-28 17:08:34 -0500

    job0

--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
 """Reduce 0."""
+
+import sys
+
+TOTAL_COUNT = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    key, value = line.split('\t')
+    count = int(value)
+    TOTAL_COUNT += count
+
+print(TOTAL_COUNT)
