62f3bbb40cf7240ec2fa53ce915917c59b02e5f4
Ayusha Subedi <ayusub@umich.edu>
2024-12-03 19:15:56 -0500

    index_server passing

--- index_server/index/__init__.py
+++ index_server/index/__init__.py
@@ -1,8 +1,9 @@
-import flask
+"""Initialie index."""
 import os
+import flask
 app = flask.Flask(__name__)
 
 app.config["INDEX_PATH"] = os.getenv("INDEX_PATH", "inverted_index_1.txt")
 
-import index.api
+import index.api  # noqa: E402  pylint: disable=wrong-import-position
 index.api.load_index()
--- index_server/index/api/main.py
+++ index_server/index/api/main.py
@@ -1,50 +1,47 @@
 """Main index."""
-
-import flask
-import index
-import json
 import re
 import sys
 import math
 import os
 from collections import Counter
+import flask
+import index
 
 inverted_index = {}
 pagerank = {}
 stopwords = set()
 
-#load index here
+
+# load index here
 def load_index():
     """Load the inverted index,stopwords, pagerank."""
     path_inverted_index = os.path.join(
         "./index_server/index/inverted_index", index.app.config["INDEX_PATH"]
     )
-    # print(path_inverted_index, file=sys.stderr)
+
     path_stopwords = os.path.join("./index_server/index/stopwords.txt")
 
     path_pagerank = os.path.join("./index_server/index/pagerank.out")
-    
-   
-    #open and add to dict 
-    with open(path_inverted_index,"r", encoding='utf-8') as f:
+
+    # open and add to dict
+    with open(path_inverted_index, "r", encoding='utf-8') as f:
         for line in f:
-                vals = [val.strip() for val in line.split()]
-                term = vals[0]
-                # print(line[1], file=sys.stderr)
-                idf = float(vals[1])
-                docs_info = {}
-                for i in range(2, len(vals), 3):  # Start at index 2 and step by 3
-                    doc_id = vals[i]
-                    tf = float(vals[i + 1])
-                    norm = float(vals[i + 2])
-                    docs_info[doc_id] = {"tf": tf, "norm": norm}
-                inverted_index[term] = {"idf": idf, "docs": docs_info}
-                
+            vals = [val.strip() for val in line.split()]
+            term = vals[0]
+            # print(line[1], file=sys.stderr)
+            docs_info = {}
+            for i in range(2, len(vals), 3):
+                doc_id = vals[i]
+                docs_info[doc_id] = {
+                    "tf": float(vals[i + 1]),
+                    "norm": float(vals[i + 2])
+                }
+            inverted_index[term] = {"idf": float(vals[1]), "docs": docs_info}
+
     with open(path_stopwords, "r", encoding='utf-8') as f:
-        global stopwords
-        stopwords = set(word.strip() for word in f)
-    
-    with open(path_pagerank, "r", encoding ='utf-8') as f:
+        stopwords.update(word.strip() for word in f)
+
+    with open(path_pagerank, "r", encoding='utf-8') as f:
         for line in f:
             doc_id, rank = line.split(',')
             pagerank[doc_id] = float(rank)
@@ -52,6 +49,7 @@ def load_index():
 
 @index.app.route('/api/v1/', methods=["GET"])
 def get_index():
+    """Return a list of services available."""
     context = {
         "hits": "/api/v1/hits/",
         "url": "/api/v1/"
@@ -67,45 +65,38 @@ def get_hits():
     weight = flask.request.args.get("w", type=float, default=0.5)
 
     terms = query.split()
-    terms = [re.sub(r"[^a-zA-Z0-9]+", "", term).casefold() for term in terms if re.sub(r"[^a-zA-Z0-9]+", "", term).strip()]
-    terms = [term.casefold() for term in terms if term.casefold() not in stopwords]
+    terms = [
+        re.sub(r"[^a-zA-Z0-9]+", "", term).casefold()
+        for term in terms if re.sub(r"[^a-zA-Z0-9]+", "", term).strip()
+    ]
+    terms = [term for term in terms if term not in stopwords]
     # print(f"Filtered terms: {terms}", file=sys.stderr)
-    # print(f"Stopwords loaded: {stopwords}", file=sys.stderr)  
+    # print(f"Stopwords loaded: {stopwords}", file=sys.stderr)
     if not all(term in inverted_index for term in terms):
         print("ERROR: terms not in inverted index", file=sys.stderr)
         return flask.jsonify({"hits": []})
-    # to check for IDF = 0????? terms = [term for term in query_terms if term in inverted_index and inverted_index[term]['idf'] > 0]
 
-    #both terms are in document 
-    doc_sets = [set(inverted_index[term]["docs"].keys()) for term in terms]
-    documents = set.intersection(*doc_sets)
+    terms = [
+        term for term in terms if term in inverted_index
+        and inverted_index[term]['idf'] != 0
+    ]
 
+    documents = get_documents(terms)
     if not documents:
-        print("ERROR: no docs", file=sys.stderr)
         return flask.jsonify({"hits": []})
-    
-    query_vector = calculate_query_vector(terms)
-    doc_nf = {}
-    for term in terms:
-        for doc_id in inverted_index[term]["docs"]:
-            doc_nf[doc_id] = math.sqrt(inverted_index[term]["docs"][doc_id]["norm"])
 
-    for doc_id in documents:
-        doc_vector = calculate_doc_vector(doc_id, terms)
-        dot_product = calc_cosine_similarity(query_vector, doc_vector)
-        query_nf = math.sqrt(sum(pow(term, 2) for term in query_vector))
-        tf_idf_score = dot_product / (abs(query_nf) * abs(doc_nf[doc_id]))
-        rank = pagerank.get(doc_id, 0)
-        final_score = (weight * rank) + ((1 - weight) * tf_idf_score)
-        hits.append({"docid": int(doc_id), "score": final_score})
-
-    hits = sorted(hits, key=lambda x: x["score"], reverse=True)
-    hits = hits[:min(10, len(hits))]
+    hits = calculate_scores(terms, documents, weight)
     return flask.jsonify({"hits": hits})
 
 
+def get_documents(terms):
+    """Find documents containing all query terms."""
+    doc_sets = [set(inverted_index[term]["docs"].keys()) for term in terms]
+    return set.intersection(*doc_sets)
+
+
 def calculate_query_vector(terms):
-    """Normalize query vector"""
+    """Normalize query vector."""
     # print("calc query norm", file=sys.stderr)
     query_vector = []
     term_frequencies = Counter(terms)
@@ -113,9 +104,9 @@ def calculate_query_vector(terms):
         if term in inverted_index:
             idf = inverted_index[term]['idf']
             query_vector.append(float(q_tf) * float(idf))
-    return query_vector 
+    return query_vector
 
-    
+
 def calculate_doc_vector(doc_id, terms):
     """Normalize document vector."""
     # print("calc doc norm", file=sys.stderr)
@@ -126,9 +117,31 @@ def calculate_doc_vector(doc_id, terms):
             idf = inverted_index[term]['idf']
             doc_vector.append(tf * idf)
     return doc_vector
-    
+
 
 def calc_cosine_similarity(query_vector, doc_vector):
     """Compute the cosine similarity between query and document vectors."""
     # print("cos", file=sys.stderr)
     return sum(q * d for q, d in zip(query_vector, doc_vector))
+
+
+def calculate_scores(terms, documents, weight):
+    """Calculate scores for relevant documents."""
+    query_vector = calculate_query_vector(terms)
+    doc_nf = {
+        doc_id: math.sqrt(inverted_index[term]["docs"][doc_id]["norm"])
+        for term in terms for doc_id in inverted_index[term]["docs"]
+    }
+
+    hits = []
+    for doc_id in documents:
+        doc_vector = calculate_doc_vector(doc_id, terms)
+        dot_product = calc_cosine_similarity(query_vector, doc_vector)
+        query_nf = math.sqrt(sum(pow(term, 2) for term in query_vector))
+        tf_idf_score = dot_product / (abs(query_nf) * abs(doc_nf[doc_id]))
+        rank = pagerank.get(doc_id, 0)
+        final_score = (weight * rank) + ((1 - weight) * tf_idf_score)
+        hits.append({"docid": int(doc_id), "score": final_score})
+
+    hits.sort(key=lambda x: x["score"], reverse=True)
+    return hits[:10]
